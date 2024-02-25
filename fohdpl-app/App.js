9b0165e9b834e15e2536ee4d9c2129cf81f02509
rajoshi <rajoshibasu@gmail.com>
2024-02-25 15:40:20 -0800

    Added button

--- fohdpl-app/App.js
+++ fohdpl-app/App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Welcome Friends of the Hoover Durant Public Library!</Text>
+      <Button
+        title = "Sign up!"
+        onPress={Login.js}
+      />
       <StatusBar style="auto" />
       {/* <div>
         <Welcome />
@@ -20,5 +24,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
 });
