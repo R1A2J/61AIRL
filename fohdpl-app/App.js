d271a0114777518cf3e116ea9cf754993da8978e
rajoshi <rajoshibasu@gmail.com>
2024-02-25 14:12:07 -0800

    1st step of Welcome

--- fohdpl-app/App.js
+++ fohdpl-app/App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Welcome from './Welcome.js'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    {/* <View style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+    </View> */}
+    <div>
+      <Welcome/>
   );
 }
 
---
+++ fohdpl-app/Welcome.js
@@ -0,0 +1,3 @@
+function Welcome() {
+    return <h1>Welcome Friends of the Hoover Durant Public Library! </h1>
+}
