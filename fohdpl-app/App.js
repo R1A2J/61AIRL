4cd956c5289034d8a318c5cc3c546e755e18ef0a
rajoshi <rajoshibasu@gmail.com>
2024-02-25 15:55:48 -0800

    added navigation capabilities

--- fohdpl-app/App.js
+++ fohdpl-app/App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import * as React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome Friends of the Hoover Durant Public Library!</Text>
-      <Button
-        title = "Sign up!"
-        onPress={Login.js}
-      />
-      <StatusBar style="auto" />
-      {/* <div>
-        <Welcome />
-      </div> */}
-    </View> 
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Welcome Friends of the Hoover Durant Public Library!</Text>
+        <Button
+          title = "Sign up!"
+          onPress={Login.js}
+        />
+        <StatusBar style="auto" />
+        {/* <div>
+          <Welcome />
+        </div> */}
+      </View> 
+    </NavigationContainer>
   );
 }
 
@@ -24,6 +27,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-
   },
 });
