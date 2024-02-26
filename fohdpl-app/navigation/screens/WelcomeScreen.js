c97de8c8242a4139d05ab50f25d0d638dd84765d
rajoshi <rajoshibasu@gmail.com>
2024-02-25 18:57:37 -0800

    madde some eddits

--- fohdpl-app/navigation/screens/WelcomeScreen.js
+++ fohdpl-app/navigation/screens/WelcomeScreen.js
@@ -1,17 +1,21 @@
 import * as React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
+const ImageComponent = () => {const image = require('.../assets/Logo_1.png')}
+
 export default function WelcomeScreen({navigation}) {
     return (
         <View style={styles.container}>
             <Text style = {{fontWeight: 'bold', fontSize: 26}}>
             Welcome Friends of the Hoover Durant Public Library!
             </Text>
+            <Image source = {image} style = {{width: 200, height: 200}} />
             <Button
                 title="Sign up!"
                 onPress={() =>
                     navigation.navigate(loginName)
                 }></Button>
+            
         </View> 
     )
 }
