d446452a36501467b645f78fd2c84a862041dc34
rajoshi <rajoshibasu@gmail.com>
2024-02-25 23:23:33 -0800

    changing to stack navigator

--- fohdpl-app/navigation/MainContainer.js
+++ fohdpl-app/navigation/MainContainer.js
@@ -1,8 +1,9 @@
+import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
 import { View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {SimpleLineIcons} from 'react-native-vector-icons/Ionicons'
+//import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+//import {SimpleLineIcons} from 'react-native-vector-icons/Ionicons';
 
 //Screens
 import WelcomeScreen from './screens/WelcomeScreen';
